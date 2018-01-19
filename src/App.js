50aa04546eaf65d424c13d994697c2dfe2444b10
artem <artem.kryvko@gmail.com>
2018-01-19 17:01:27 +0200

    The first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <span>Hello!</span>
+      </div>
+    );
+  }
+}
+
+export default App;
